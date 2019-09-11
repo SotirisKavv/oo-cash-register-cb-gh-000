9f9f9d28f84ee434065f0c2e26e25fcf33ef8f74
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 13:23:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
 
+  attr_accessor :total, :employee_discount
+
+  def initialize(employee_discount)
+    @total = 0
+    @employee_discount = employee_discount
+  end
+
+  def initialize
+    @total = 0
+  end
+end
