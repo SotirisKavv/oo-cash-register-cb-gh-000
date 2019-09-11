d3cdb8f836c37975d19db7d6a8aab93461a21913
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 13:35:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,16 @@
 class CashRegister
 
-  attr_accessor :total, :discount
-
+  attr_accessor :total, :discount, :list
+  
   def initialize(employee_discount = 0)
     @total = 0
     @discount = employee_discount
+    @list = {}
   end
 
+  def add_item(title, price, quantity = 1)
+    list[title] = price
+    @total += price * quantity
+  end
 
 end
