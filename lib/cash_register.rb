6e730b8e2794153d6eb60dc86faf0be7e5298347
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 13:45:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   end
 
   def apply_discount
-    @total -= @discount/100 * @total
+    @total *= (100 - @discount) / 100
     puts "After the discount, the total comes to $#{@total}"
   end
 
